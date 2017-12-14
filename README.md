50d0961457a58015c3422539320dfc18741f4d7d
Jakub Zurawski <jakub.zrw@gmail.com>
2017-12-14 17:27:11 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # resilient-eureka
-Resilient HTTP middleware for Netflix Eureka discovery service
+[resilient.js](https://github.com/resilient-http/resilient.js) middleware for [Netflix Eureka](https://github.com/Netflix/eureka) discovery service
+
+
+## Installation
+
+```
+npm install resilient-eureka --save
+```
+
+## Usage
+
+```js
+const Resilient = require('resilient')
+const eurekaMiddleware = require('resilient-eureka')
+
+const client = Resilient()
+
+//eureka urls
+const servers = [
+  'http://demo.eureka.com'
+]
+
+client.use(eurekaMiddleware({
+  serviceName: 'service'
+}))
+
+client.setServers(servers)
+
+// Test request
+client.get('/', function (err, res) {
+  if (res.status === 200) {
+    console.log('Response:', res)
+  }
+})
+```
+
+## Params
+
+- **serviceName/serviceVipAddress** `string` - name/vipAddress of service registered in Eureka (one of these is required)
+- **path** `string` - path to query for all Eureka's services instances (default: `/eureka/apps`)
+- **protocol** `string` - transport protocol (default: `http`)
---
+++ eureka.js
@@ -0,0 +1,38 @@
+const xml2js = require('xml2js')
+
+module.exports = function eurekaMiddleware(params) {
+  params = params || {}
+
+  function middleware(options, resilient) {
+    options.set('path', params.path || '/eureka/apps')
+    return {
+      'in': function (err, res, next) {
+        if (err) return next()
+        xml2js.parseString(res.body, (err, result) => {
+          if (err) return next()
+          res.data = mapServersFromEurekaResponse(result)
+          next() 
+        })
+      },
+      'out': function (options, next) {
+        next()
+      }
+    }
+  }
+  
+  function mapServersFromEurekaResponse(data) {
+    return data
+      .applications
+      .application
+      .filter(service => params.serviceName ? service.name[0] === params.serviceName : true)
+      .map(service => service.instance)
+      .reduce((flat, toFlatten) =>
+        flat.concat(toFlatten), [])
+      .filter(service => params.serviceVipAddress ? service.vipAddress[0] === params.serviceVipAddress : true)
+      .map(service => params.protocol || 'http' + '://' + service.ipAddr[0] + ':' + service.port[0]._)
+  }
+  
+  middleware.type = 'discovery' 
+  
+  return middleware
+}
